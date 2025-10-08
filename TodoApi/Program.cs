65735ecd6ee8a97a6dc449dbb84ba0b7d8204695
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-10-08 09:29:11 -0600

    custom docs via comment working
    added options to swagger gen in program.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using TodoApi.Data;
 using TodoApi.Repositories;
 using TodoApi.Services;
@@ -18,9 +19,16 @@ builder.Services.AddScoped<ITodoService, TodoService>();
 builder.Services.AddControllers();
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Swagger stuff
+builder.Services.AddSwaggerGen( options =>
+{
+    //reading xml from here(?
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
@@ -33,37 +41,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-
-// ===================== TEMPLATE STUFF =====================
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-// ===================== TEMPLATE STUFF =====================
-
 app.MapControllers();
 
 app.Run();
-
-// ===================== TEMPLATE STUFF =====================
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-// ===================== TEMPLATE STUFF =====================
